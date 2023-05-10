64bcd96c39dd63341ea3592867a149c3259be01d
Dendrose <Dendrose10@gmail.com>
2023-05-10 12:52:04 +0200

    scrollable to torch
    so the torch can be scrolled to see further but narrows the light and vice versa

--- Assets/scripts/FlashLight.cs
+++ Assets/scripts/FlashLight.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlashLight : MonoBehaviour
@@ -10,17 +11,23 @@ public class FlashLight : MonoBehaviour
 
     private Light Torchlight;
 
+    [SerializeField] double MaxLightRange = 0.001;
+    [SerializeField] double MinLightRange = -0.001;
+
     // Start is called before the first frame update
     void Start()
-    {       
+    {      
+        
         Torchlight = this.GetComponent<Light>();
-        Torchlight.enabled = false;
+        Torchlight.enabled = false;      
     }
 
     // Update is called once per frame
     //Turning torch on and off with two functions
     void Update()
     {
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+
         if (Input.GetKeyDown(KeyCode.F)) 
         {
             Debug.Log("Hello from Flashlight");
@@ -34,6 +41,21 @@ public class FlashLight : MonoBehaviour
             }
         
         }
+        if (scrollInput > 0 && Torchlight.range < MaxLightRange)
+        {
+            Torchlight.spotAngle --;
+            Torchlight.range++;
+           
+            Debug.Log("MouseWheel");
+        }
+        if (scrollInput < 0 && Torchlight.range > MinLightRange)
+        {
+            Torchlight.spotAngle++;
+            Torchlight.range--;        
+        }
+
+
+        
     }
 
     //Onfunction
