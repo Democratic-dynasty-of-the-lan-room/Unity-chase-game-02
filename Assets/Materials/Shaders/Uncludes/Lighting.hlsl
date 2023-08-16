0ee021c2b6eb57cc193f87824fb3e321a79d6bdf
Dragonking303 <63424175+Dragonking303@users.noreply.github.com>
2023-08-16 18:30:49 +0200

    Texture stuffs
    Old Changes

--- Assets/Materials/Shaders/Uncludes/Lighting.hlsl
+++ Assets/Materials/Shaders/Uncludes/Lighting.hlsl
@@ -2,6 +2,7 @@
 #define CUSTOM_LIGHTING_INCLUDED
 
 void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out half2 DistanceAtten, out half2 ShadowAtten) 
+
 {
 #if SHADERGRAPH_PREVIEW
 	Direction = half3(0.5 0.5, 0);
@@ -23,3 +24,29 @@ void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 
 #endif
 }
 #endif
+
+void AddAdditionalLights_float(float Smoothness, float WorldPosition, float WorldNormal, float3 WorldView, float MainDiffuse, float MainSpecular, float3 MainColor
+	, out float Diffuse, out float Specular out float3 Color)
+	{
+	Diffuse = MainDiffuse;
+	Specular = MainSpecular;
+	Color = MainColor * (MainDiffuse + MainSpecular);
+
+	#ifndef SHADERGRAPH_PREVIEW
+	int pixelLightCount = GetAdditionalLightsCount();
+	for (int i=0; i < pixelLightCount; ++i){
+		Light light = GetAdditionalLight(i, WorldPosition);
+		half NdotL = saturate(dot(WorldNormal, light.direction));
+		half atten = light.distanceAttenuation * light.shadowAttenuation;
+		half thisDiffuse = atten * NdotL;
+		half thisSpecular = LightingSpecular(thisDiffuse, light.direction, WorldNormal, WorldView, 1, Smoothness);
+		Diffuse += thisDiffuse;
+		Specular += thisSpecular;
+		Color += light.color * (thisDiffuse + thisSpecular);
+		}
+		#endif
+
+		half total = Diffuse + Specular;
+		//If no lights touches this pixel , set the color to the main light's color
+		Color = total <= 0 ? MainColor : Color / total;
+	}
